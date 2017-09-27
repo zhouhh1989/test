9b145c8261f476f0c7687a7f36d536d69e028d1d
zhouhonghua <312811643@qq.com>
2017-09-27 22:32:06 +0800

    ss

---
+++ test/app/Http/Controllers/Test.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Jobs\TestJob;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
+
+class Test extends Controller
+{
+    public function index()
+    {
+        for($i = 0; $i <10; $i++) {
+            dispatch(new TestJob());
+            Storage::put('test.txt',$i);
+        }
+    }
+}
