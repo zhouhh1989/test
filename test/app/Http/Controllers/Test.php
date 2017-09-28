dc70cfff05b5b8c5fa14c3d286c681b0244026c3
zhouhonghua <312811643@qq.com>
2017-09-29 00:02:00 +0800

    gg

--- test/app/Http/Controllers/Test.php
+++ test/app/Http/Controllers/Test.php
@@ -5,17 +5,20 @@ namespace App\Http\Controllers;
 use App\Jobs\TestJob;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\Redis;
 
 class Test extends Controller
 {
 
     public function index()
     {
+        Redis::set('ss','ss');
 
 //        for($i = 0; $i <10; $i++) {
-            dispatch(new TestJob());
+//            dispatch(new TestJob());
 //            Storage::put('test.txt',$i);
 //        }
+
         return [];
     }
 }
--- test/app/Jobs/TestJob.php
+++ test/app/Jobs/TestJob.php
@@ -9,6 +9,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Maatwebsite\Excel\Facades\Excel;
 
+
 class TestJob implements ShouldQueue
 {
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
